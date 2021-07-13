76dffa9d5b598f1879cd5d0efeb2fbee0b52f1ff
ricardo.bermudez <ricardo.bermudez@iesonline.com.co>
2021-07-13 12:03:22 -0500

    feat: servicio de vincular maquina

---
+++ src/servicios/VincularMaquina.ts
@@ -0,0 +1,36 @@
+import Https from 'https';
+import axios, { AxiosResponse, AxiosError } from 'axios';
+
+export const VincularMaquina = (arg) => {
+  let data = {
+    "nombreServicio":"iniciarFidelizacion",
+    "numeroDeParametros":"1",
+    "parametros":[
+        {
+            "nombreParametro":"serial",
+            "valorParametro": `${arg.serial}`
+        }
+    ]
+  };
+
+  var config = {
+    method: 'post',
+    url: `https://${arg.host}:8443/MobilAppV2/fidelizacion/iniciarFidelizacion`,
+    httpsAgent: new Https.Agent({ rejectUnauthorized: false }),
+    agent: false,
+    strictSSL: false,
+    headers: {
+      'Content-Type': 'application/json',
+      token: `${arg.token}`,
+    },
+    data: data,
+  };
+
+  return axios(config)
+    .then((res: AxiosResponse) => {
+      return res.data;
+    })
+    .catch((error: AxiosError) => {
+      console.log(error);
+    });
+};
