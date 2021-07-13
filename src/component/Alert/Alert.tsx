45e2e5a2a97bbdbd3eedda0116bacc9fcf2a06e4
ricardo.bermudez <ricardo.bermudez@iesonline.com.co>
2021-07-13 12:03:40 -0500

    feat: componente para mostrar alertas

---
+++ src/component/Alert/Alert.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import { Button } from '@material-ui/core';
+
+export default function Alert({ open, onClose, message }) {
+  return (
+    <Dialog
+      open={open}
+      onClose={onClose}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <DialogTitle id="alert-dialog-title">{'Error'}</DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          {message}
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose} variant="contained" color="primary">
+          Cerrar
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
