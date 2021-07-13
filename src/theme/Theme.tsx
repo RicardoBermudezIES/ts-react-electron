170ff2e9b4ebe3be376b95061809552409392c43
ricardo.bermudez <ricardo.bermudez@iesonline.com.co>
2021-07-13 12:03:02 -0500

    fix: se cambio el componente que crea eltema

--- src/theme/Theme.tsx
+++ src/theme/Theme.tsx
@@ -1,7 +1,7 @@
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createTheme } from '@material-ui/core/styles';
 import { pink, lightBlue } from '@material-ui/core/colors/';
 
-export const theme = createMuiTheme({
+export const theme = createTheme({
   palette: {
     primary: {
       main: pink[500],
