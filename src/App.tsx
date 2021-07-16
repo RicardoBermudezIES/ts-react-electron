4bb7b67ff77bd1bd528d0ba52ca65cee3f5d55b7
ricardo.bermudez <ricardo.bermudez@iesonline.com.co>
2021-07-16 08:34:53 -0500

    feat: se piden tokens nuevos cada minuto

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,58 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.global.css';
 import fs from 'fs';
 import paths from 'path';
 import { ThemeProvider } from '@material-ui/core';
+import { ipcRenderer } from 'electron';
 import { theme } from './theme/Theme';
 import { router } from './router';
 import DataProvider from './context/Context';
 
-
-// const Hello = () => {
-//   return (
-//     <div>
-//       <Box className="Hello">
-//         <img width="200px" alt="icon" src={icon} />
-//       </Box>
-//       <h1>electron-react-boilerplate</h1>
-//       <div className="Hello">
-//         <Button
-//           variant="contained"
-//           color="primary"
-//           type="button"
-//           onClick={() => {
-//             let archivo = fs.readFileSync(
-//               paths.resolve(__dirname, './config.json'),
-//               'utf-8'
-//             );
-//             console.log(JSON.parse(archivo));
-//             console.log(
-//               'Esto se muestra después de haber leído el achivo2.txt por el readFileSync'
-//             );
-//           }}
-//         >
-//           <span role="img" aria-label="books">
-//             📚
-//           </span>
-//           Read our docs
-//         </Button>
-//         <button type="button">
-//           <span role="img" aria-label="books">
-//             🙏
-//           </span>
-//           Donate
-//         </button>
-//       </div>
-//     </div>
-//   );
-// };
+const ipc = ipcRenderer;
 
 export default function App() {
 
+  const [token] = useState(JSON.parse(localStorage.getItem('authConfig')) ? JSON.parse(localStorage.getItem('authConfig')) : '')
+
+  const sendAuth = () => {
+    const auth = JSON.parse(localStorage.getItem('authConfig'));
+    ipc.send('allways-auth', auth);
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem('authConfig')) {
+      setInterval(() => {
+        sendAuth();
+      }, 1000 * 60)
+    }
+  },[token]);
+
+
+  const getAuth = () => {
+    ipc.on('allways-auth', (event, arg) => {
+        console.log(arg.token)
+        localStorage.setItem('token', arg.token);
+    });
+  };
+
+  useEffect(() => {
+    getAuth();
+  },[]);
+
+
+
   return (
     <DataProvider>
-    <Router>
-      <ThemeProvider theme={theme}>
-        <Switch>
-          {router.map((route, i) => (
-            <Route key={i} path={route.path}  component={route.component} />
-          ))}
-        </Switch>
-      </ThemeProvider>
-    </Router>
+      <Router>
+        <ThemeProvider theme={theme}>
+          <Switch>
+            {router.map((route, i) => (
+              <Route key={i} path={route.path} component={route.component} />
+            ))}
+          </Switch>
+        </ThemeProvider>
+      </Router>
     </DataProvider>
   );
 }
