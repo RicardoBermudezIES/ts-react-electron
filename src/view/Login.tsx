f58a59b305e9c24e4d292d3cbdea229fee9fb9bc
ricardo.bermudez <ricardo.bermudez@iesonline.com.co>
2021-07-21 17:08:00 -0500

    feat: se agregan e guion al layout del teclado

--- src/view/Login.tsx
+++ src/view/Login.tsx
@@ -355,10 +355,10 @@ function Login() {
             default: [
               "1 2 3 4 5 6 7 8 9 0",
               "q w e r t y u i o p",
-              "a s d f g h j k l",
+              "a s d f g h j k l -",
               "{shift1} z x c v b n m {bksp}"
             ],
-            shift: ["1 2 3", "4 5 6", "7 8 9", "{shift} 0 {bksp}"],
+            shift: ["1 2 3", "4 5 6", "7 8 9", "{shift} 0 - {bksp}"],
 
           }}
           display={{
