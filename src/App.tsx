27fcca9ed852f337e377018a04fa9df88f540858
ricardo.bermudez <ricardo.bermudez@iesonline.com.co>
2021-07-21 11:14:57 -0500

    feat: se aplica el logo de juego resonsable

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.global.css';
 import fs from 'fs';
 import paths from 'path';
-import { ThemeProvider } from '@material-ui/core';
+import { Box, ThemeProvider } from '@material-ui/core';
 import { ipcRenderer } from 'electron';
 import { theme } from './theme/Theme';
 import { router } from './router';
 import DataProvider from './context/Context';
+import Juego from './iconos/JuegoResponsable.svg'
 
 const ipc = ipcRenderer;
 
@@ -44,6 +45,7 @@ export default function App() {
     <DataProvider>
       <Router>
         <ThemeProvider theme={theme}>
+          <Box position="absolute" bottom="5px" right="5px"> <img style={{width:100}} src={Juego} alt="juego Responsable" /> </Box>
           <Switch>
             {router.map((route, i) => (
               <Route key={i} path={route.path} component={route.component} />
