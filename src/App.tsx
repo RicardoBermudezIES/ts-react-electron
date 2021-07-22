334259f50d754c93387a8eed2ed2f3a40668eb03
ricardo.bermudez <ricardo.bermudez@iesonline.com.co>
2021-07-22 15:02:49 -0500

    feat: se muestra la alerta si no esta conectado a internet

--- src/App.tsx
+++ src/App.tsx
@@ -8,38 +8,48 @@ import { ipcRenderer } from 'electron';
 import { theme } from './theme/Theme';
 import { router } from './router';
 import DataProvider from './context/Context';
-import Juego from './iconos/JuegoResponsable.svg'
+import Juego from './iconos/JuegoResponsable.svg';
+import Alert from './component/Alert/Alert';
 
 const ipc = ipcRenderer;
 
 export default function App() {
+  const [open, setOpen] = useState(!navigator.onLine);
 
-  const sendAuth = () => {
-    const auth = JSON.parse(localStorage.getItem('authConfig'));
-    ipc.send('allways-auth', auth);
+
+  const isOnline = () => {
+    navigator.onLine ? setOpen(false) : setOpen(true);
   };
 
   useEffect(() => {
+    setInterval(() => {
+      isOnline();
+    }, 1000 * 60 * 1);
+  }, []);
+
+  useEffect(() => {
+    const sendAuth = () => {
+      const auth = JSON.parse(localStorage.getItem('authConfig'));
+      ipc.send('allways-auth', auth);
+    };
+
     if (localStorage.getItem('authConfig')) {
       setInterval(() => {
         sendAuth();
-      }, 1000 * 60 * 4)
+      }, 1000 * 60 * 4);
     }
-  },[]);
-
+  }, []);
 
   const getAuth = () => {
     ipc.on('allways-auth', (event, arg) => {
-        console.log(arg.token)
-        localStorage.setItem('token', arg.token);
+      console.log(arg.token);
+      localStorage.setItem('token', arg.token);
     });
   };
 
   useEffect(() => {
     getAuth();
-  },[]);
-
-
+  }, []);
 
   return (
     <DataProvider>
@@ -48,6 +58,14 @@ export default function App() {
           <Box position="absolute" bottom="5px" right="5px">
             <img style={{ width: 150 }} src={Juego} alt="juego Responsable" />
           </Box>
+
+          {navigator.onLine ? null : (
+            <Alert
+              open={open}
+              onClose={() => setOpen(false)}
+              message="No hay conexion al internet"
+            />
+          )}
           <Switch>
             {router.map((route, i) => (
               <Route key={i} path={route.path} component={route.component} />
