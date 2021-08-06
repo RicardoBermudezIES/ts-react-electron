7ef0775158ab4c63baf03ce015f28fcb48622793
ricardo.bermudez <ricardo.bermudez@iesonline.com.co>
2021-08-06 16:23:48 -0500

    feat: se aumenta el tamaño de los iconos

--- src/component/NavButton.tsx
+++ src/component/NavButton.tsx
@@ -1,4 +1,4 @@
-import { Button, Grid, makeStyles } from '@material-ui/core';
+import { Button, Grid, makeStyles, Typography } from '@material-ui/core';
 import React from 'react';
 import { useHistory } from 'react-router';
 import {
@@ -8,14 +8,12 @@ import {
   AccountBalanceWallet,
   Style,
 } from '@material-ui/icons';
+import { BarIcon } from '../iconos/Bar';
+import { PuntosIcon } from '../iconos/PuntosIcon';
+import { PhoneIcon } from '../iconos/PhoneIcon';
+import { BilleteraIcon } from '../iconos/BilleteraIcon';
 
-const useStyles = makeStyles(() => ({
-  disabled: {
-    '&.MuiButton-root.Mui-disabled': {
-      backgroundColor: 'rgba(233,30,99,0.5) !important',
-    },
-  },
-}));
+const useStyles = makeStyles(() => ({}));
 
 export default function NavButton() {
   const classes = useStyles();
@@ -29,52 +27,53 @@ export default function NavButton() {
     history.push('/puntos');
   };
   return (
-    <Grid justify="flex-end" container spacing={2}>
-      <Grid item>
-        <Grid container alignItems="flex-end" direction="column" spacing={2}>
-          <Grid item>
-            <Button size="large" onClick={goPuntos} variant="contained" color="primary">
-              <ScatterPlot style={{ fontSize: 40 }} />
-            </Button>
+    <>
+      <Grid justify="flex-end" container spacing={2}>
+        <Grid item>
+          <Grid container alignItems="flex-end" direction="column" spacing={2}>
+            <Grid item>
+              <Button style={{ display: 'grid' }} onClick={goPuntos}>
+                <PuntosIcon />
+                <Typography style={{ color: 'white' }}>
+                  {' '}
+                  Puntos{' '}
+                </Typography>
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button style={{ display: 'grid', opacity: 0.5 }}>
+                <BilleteraIcon />
+                <Typography style={{ color: 'white' }}>
+                  {' '}
+                  Billtera{' '}
+                </Typography>
+              </Button>
+            </Grid>
           </Grid>
-          <Grid item>
-            <Button
-              size="large"
-              disabled
-              className={classes.disabled}
-              variant="contained"
-              color="primary"
-            >
-              <AccountBalanceWallet style={{ fontSize: 40 }} />
-            </Button>
-          </Grid>
-          <Grid item>
-            <Button
-              size="large"
-              className={classes.disabled}
-              disabled
-              variant="contained"
-              color="primary"
-            >
-              <Style  style={{ fontSize: 40 }} />
-            </Button>
+        </Grid>
+        <Grid item>
+          <Grid container alignItems="flex-end" direction="column" spacing={3}>
+            <Grid item>
+              <Button style={{ display: 'grid' }} disabled>
+                <PhoneIcon />
+                <Typography style={{ color: 'white' }}>
+                  {' '}
+                  Ayuda{' '}
+                </Typography>
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button onClick={goBar} style={{ display: 'grid' }}>
+                <BarIcon />
+                <Typography style={{ color: 'white' }}>
+                  {' '}
+                  Bar{' '}
+                </Typography>
+              </Button>
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
-      <Grid item>
-        <Grid container alignItems="flex-end" direction="column" spacing={2}>
-          <Grid item>
-            <Button size="large" variant="contained" color="primary">
-            <CallEndTwoTone  style={{ fontSize: 40 }} />
-            </Button>
-          </Grid>
-          <Grid item>
-            <Button size="large"  onClick={goBar} variant="contained" color="primary">
-            <Fastfood  style={{ fontSize: 40 }} />
-            </Button>
-          </Grid>
-        </Grid>
-      </Grid>
-    </Grid>
+    </>
   );
 }
