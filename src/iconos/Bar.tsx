3449e59d1130616bed10892462040f6773825422
ricardo.bermudez <ricardo.bermudez@iesonline.com.co>
2021-08-06 16:23:36 -0500

    feat: se crean los inconos

---
+++ src/iconos/Bar.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export const BarIcon = (props: Props) => {
+  return (
+    <svg
+      width="80"
+      height="80"
+      viewBox="0 0 60 60"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M45.2471 18.0093V14.5815H14.3968V18.0093L28.108 33.4344V42.004H19.5385V45.4318H40.1054V42.004H31.5358V33.4344L45.2471 18.0093ZM21.9894 21.4371L18.9558 18.0093H40.7052L37.6545 21.4371H21.9894Z"
+        fill="#DB0380"
+      />
+      <rect
+        x="1"
+        y="1.18469"
+        width="57.6439"
+        height="57.6439"
+        rx="8"
+        stroke="#DB0380"
+        stroke-width="2"
+      />
+    </svg>
+  );
+};
