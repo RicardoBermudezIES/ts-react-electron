42496331778700cc369bdb8452a6ccb7572a2db3
ricardo.bermudez <ricardo.bermudez@iesonline.com.co>
2021-08-20 09:34:01 -0500

    feat: se crea el servicio para solicitar los productos del bar

---
+++ src/servicios/bar.ts
@@ -0,0 +1,34 @@
+import Https from 'https';
+import axios, { AxiosResponse, AxiosError } from 'axios';
+
+export const barServices = (arg) => {
+  const { host, casino, token } = arg;
+  let data = {
+    nombreServicio: 'visualizarPremiosBar',
+    numeroDeParametros: '2',
+    parametros: [
+      { nombreParametro: 'codigoCasino', valorParametro: casino },
+      { nombreParametro: 'cargaTodo', valorParametro: 'si' },
+    ],
+  };
+  var config = {
+    method: 'post',
+    url: `https://${host}:8443/MobilAppV2/bar/visualizarPremiosBar'`,
+    httpsAgent: new Https.Agent({ rejectUnauthorized: false }),
+    agent: false,
+    strictSSL: false,
+    headers: {
+      'Content-Type': 'application/json',
+      token: `${token}`,
+    },
+    data: data,
+  };
+
+  return axios(config)
+    .then((res: AxiosResponse) => {
+      return res.data;
+    })
+    .catch((error: AxiosError) => {
+      return error;
+    });
+};
