1e16d654e72f7b0df397a052e3a5b2d555f7e890
ricardo.bermudez <ricardo.bermudez@iesonline.com.co>
2021-08-30 11:07:28 -0500

    feat: se crea un servicio para realizar peticion

---
+++ src/servicios/realizarPeticion.ts
@@ -0,0 +1,45 @@
+import Https from 'https';
+import axios, {  AxiosResponse, AxiosError } from 'axios';
+
+export const realizarPeticion = (arg) => {
+  const { host, numeroDocumento, maquina, token, puk } = arg;
+  var data = {
+    nombreServicio: 'realizarPeticion',
+    numeroDeParametros: '3',
+    parametros: [
+      {
+        nombreParametro: 'numeroDocumento',
+        valorParametro: numeroDocumento+"",
+
+      },{
+        nombreParametro: 'serial',
+        valorParametro: `${maquina}`
+      },{
+        nombreParametro: 'idPeticion',
+        valorParametro: `${puk}`
+      },
+    ],
+  };
+  var config = {
+    method: 'post',
+    url: `https://${host}:8443/MobilAppV2/fidelizacion/cerrarFidelizacionMaquina`,
+    httpsAgent: new Https.Agent({ rejectUnauthorized: false }),
+    agent: false,
+    strictSSL: false,
+    headers: {
+      'Content-Type': 'application/json',
+      token: `${token}`,
+    },
+    data: data,
+  };
+
+  return axios(config)
+    .then((res: AxiosResponse) => {
+      console.log(res.data, "servicio")
+      return res.data;
+
+    })
+    .catch((error: AxiosError) => {
+      return error
+    });
+};
