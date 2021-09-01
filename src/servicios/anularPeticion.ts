44435b3f8442189f7104b709e5cbef08f16eed7c
ricardo.bermudez <ricardo.bermudez@iesonline.com.co>
2021-09-01 15:11:30 -0500

    feat: anularPeticion.ts: Se crea el servicio para anular la peticion en el Bar

---
+++ src/servicios/anularPeticion.ts
@@ -0,0 +1,46 @@
+import Https from 'https';
+import axios from 'axios';
+
+export const anularPeticion = async (arg) => {
+  const { host, maquina, token, puk } = arg;
+  var data = {
+    nombreServicio: 'anularPeticion',
+    numeroDeParametros: '4',
+    parametros: [
+      {
+        nombreParametro: 'nota',
+        valorParametro: " ",
+
+      },{
+        nombreParametro: 'haySesion',
+        valorParametro: `JWT`
+      },{
+        nombreParametro: 'serial',
+        valorParametro: `${maquina}`
+      },
+      {
+        nombreParametro: 'idPeticion',
+        valorParametro: `${puk}`
+      },
+    ],
+  };
+  var config = {
+    method: 'post',
+    url: `https://${host}:8443/MobilAppV2/bar/anularPeticion`,
+    httpsAgent: new Https.Agent({ rejectUnauthorized: false }),
+    agent: false,
+    strictSSL: false,
+    headers: {
+      'Content-Type': 'application/json',
+      token: `${token}`,
+    },
+    data: data,
+  };
+
+  try {
+    const res = await axios(config);
+    return res.data;
+  } catch (error) {
+    return error;
+  }
+};
