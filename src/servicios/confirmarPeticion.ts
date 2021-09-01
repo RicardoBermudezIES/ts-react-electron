74e21de768f035f010f02441b075fc36f2b72a98
ricardo.bermudez <ricardo.bermudez@iesonline.com.co>
2021-09-01 15:13:08 -0500

    feat: confirmarPeticion.ts: Se crea el servicio para confirmar la peticion en el Bar

---
+++ src/servicios/confirmarPeticion.ts
@@ -0,0 +1,42 @@
+import Https from 'https';
+import axios from 'axios';
+
+export const confirmarPeticion = async (arg) => {
+  const { host, maquina, token, puk } = arg;
+  var data = {
+    nombreServicio: 'confirmarPeticion',
+    numeroDeParametros: '3',
+    parametros: [
+     ,{
+        nombreParametro: 'haySesion',
+        valorParametro: `JWT`
+      },{
+        nombreParametro: 'serial',
+        valorParametro: `${maquina}`
+      },
+      {
+        nombreParametro: 'idPeticion',
+        valorParametro: `${puk}`
+      },
+    ],
+  };
+  var config = {
+    method: 'post',
+    url: `https://${host}:8443/MobilAppV2/bar/confirmarPeticion`,
+    httpsAgent: new Https.Agent({ rejectUnauthorized: false }),
+    agent: false,
+    strictSSL: false,
+    headers: {
+      'Content-Type': 'application/json',
+      token: `${token}`,
+    },
+    data: data,
+  };
+
+  try {
+    const res = await axios(config);
+    return res.data;
+  } catch (error) {
+    return error;
+  }
+};
