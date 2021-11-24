58ced1960b27b44d1a4bc5f69302defd0889b223
ricardo bermudez <ricardobzorro@gmail.com>
2021-11-24 10:42:23 -0500

    feat:(useproduct, usePuntosDia): se agregan mas tipados a las funciones

--- src/Hook/useProduct.tsx
+++ src/Hook/useProduct.tsx
@@ -1,13 +1,16 @@
-import { ipcRenderer } from "electron";
-import { useEffect, useState } from "react";
+import { ipcRenderer } from 'electron';
+import { useEffect, useState } from 'react';
+import { Product } from '../types/Products';
+
 const ipc = ipcRenderer;
 
-export default function useProduct(){
-  const [productos, setProductos] = useState();
+export default function useProduct() {
+  const [productos, setProductos] = useState<Product[]>([]);
   const [openError, setOpenError] = useState(false);
   const [messageError, setmessageError] = useState('');
 
   const getBar = () => {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     const auth = JSON.parse(localStorage.getItem('authConfig'));
     ipc.send('allways-auth', auth);
 
@@ -20,47 +23,40 @@ export default function useProduct(){
         casino: localCasino,
         token: localToken,
       };
-        ipc.send('bar', args);
+      ipc.send('bar', args);
     }, 500);
+  };
 
-   }
+  useEffect(() => {
+    getBar();
+  }, []);
 
-   useEffect(() => {
-
-      getBar()
-
-
-   }, [])
-
-   useEffect(() => {
-    ipc.on('bar', (event, arg) => {
-
+  useEffect(() => {
+    ipc.on('bar', (_event, arg) => {
       if (arg?.statusDTO?.code !== '00') {
         // eslint-disable-next-line no-console
         setmessageError(arg?.statusDTO?.message);
         setOpenError(true);
       }
 
-      if (arg?.statusDTO?.code == '00') {
+      if (arg?.statusDTO?.code === '00') {
         localStorage.setItem(
           'bar',
           JSON.stringify(arg?.listaVisualizarPremiosDTO)
         );
-        const newSet = new Set()
-        arg?.listaVisualizarPremiosDTO.forEach(( l => newSet.add(l?.categoriaPremio)));
-        setProductos(newSet)
-
+        const newSet = new Set<string>();
+        arg?.listaVisualizarPremiosDTO.forEach((l: string) =>
+          newSet.add(l?.categoriaPremio)
+        );
+        setProductos(newSet);
       }
-
     });
-
-  },[]);
-
+  }, []);
 
   return {
     productos,
     openError,
-    messageError
-  }
-
+    messageError,
+    setOpenError,
+  };
 }
--- src/Hook/usePuntosDia.tsx
+++ src/Hook/usePuntosDia.tsx
@@ -1,13 +1,17 @@
 import { ipcRenderer } from 'electron';
 import { useEffect, useState } from 'react';
+
 const ipc = ipcRenderer;
 export default function usePuntosDia() {
-  const [puntosBar, setPuntosBar] = useState();
+  const [puntosBar, setPuntosBar] = useState(0);
 
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   const user = JSON.parse(localStorage.getItem('user'));
 
   // solicitar el puntosbar.
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   const puntosXBar = () => {
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     const auth = JSON.parse(localStorage.getItem('authConfig'));
     const localCasino = localStorage.getItem('casino');
     const localToken = localStorage.getItem('token');
@@ -26,17 +30,18 @@ export default function usePuntosDia() {
     if (user) {
       puntosXBar();
     }
-  }, []);
+  }, [puntosXBar, user]);
 
   useEffect(() => {
-    ipc.on('visualizarPuntosxDia', (event, arg) => {
+    ipc.on('visualizarPuntosxDia', (_event, arg) => {
       // eslint-disable-next-line no-console
       console.log(arg?.cantidadPuntosDisponibles, 'bar.tsx');
 
+      // eslint-disable-next-line no-empty
       if (arg?.statusDTO?.code !== '00') {
       }
 
-      if (arg?.statusDTO?.code == '00') {
+      if (arg?.statusDTO?.code === '00') {
         localStorage.setItem(
           'puntosDiaxBar',
           JSON.stringify(arg?.cantidadPuntosDisponibles)
