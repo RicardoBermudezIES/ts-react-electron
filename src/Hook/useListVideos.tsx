ea937783297d990479c52e9fd2f8bf0a7b7d0cc0
Ricardo Bermudez <ricardo.bermudez@iesonline.com.co>
2021-12-21 12:03:09 -0500

    refactor: se replantea la logica de la captra de los elementos, hay que cambiar los nombres

--- src/Hook/useListVideos.tsx
+++ src/Hook/useListVideos.tsx
@@ -1,4 +1,4 @@
-import { RefObject, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import fs from 'fs';
 import os from 'os';
 import path from 'path';
@@ -6,24 +6,19 @@ import path from 'path';
 export default function useListVideos(
   setIsVideoPlay: Function,
   isVideoPlay: boolean,
-  videoRef: RefObject<HTMLVideoElement>
 ) {
   const URL_BASE: string = path.join(os.homedir(), 'fidelizacion');
 
-  const URL = `${URL_BASE}/video-fidelizacion-`;
-  const [numberMax, setNumberMax] = useState<number>(0);
-  const [numberSong, setNumberSong] = useState(1);
-  const [hasVideos, setHasVideos] = useState(false);
+  const [listPromocional, setListPromocional] = useState<string[]>([]);
+  const [error, setError] = useState();
 
   const getListVideos = (URL_BASE: string): Promise<Array<string>> => {
     return new Promise((resolve, reject) => {
       fs.readdir(URL_BASE, { withFileTypes: true }, (err, files) => {
         if (err) {
-          setHasVideos(true);
           reject(err)
         } 
         else {
-          setHasVideos(false)
           resolve(files.map((item) => item.name));
         }
       });
@@ -32,47 +27,22 @@ export default function useListVideos(
 
   useEffect(() => {
     getListVideos(URL_BASE)
-      .then((res: Array<string>) => {
-        setNumberMax(res.length);
+      .then((res: string[]) => {
+        setListPromocional(res);
+        console.log(listPromocional);
+        
       })
-      .catch(() => {
-        getListVideos(URL_BASE).then((res: Array<string>) => {
-          setNumberMax(res.length);
-        });
+      .catch((error) => {
+          setError(error);
       });
   }, []);
 
-  const UpCounterVideo = () => {
-    setNumberSong(numberSong + 1);
-  };
-
-  const resetVideo = () => {
-    if (numberSong > numberMax) {
-      setNumberSong(1);
-    }
-  };
-
-  useEffect(() => {
-    resetVideo();
-  }, [numberSong]);
 
   const OnClickHiddenVideo = () => {
     setIsVideoPlay(!isVideoPlay);
     console.log(isVideoPlay + 'App');
-    videoRef.current!.muted = true;
-    videoRef.current?.pause();
   };
 
-  useEffect(() => {
-    if (isVideoPlay === true) {
-      videoRef.current!.muted = false;
-      videoRef.current!.play();
-    } else {
-      UpCounterVideo();
-      videoRef.current!.muted = true;
-      videoRef.current!.pause();
-    }
-  }, [isVideoPlay]);
 
-  return { hasVideos, numberSong, URL, OnClickHiddenVideo };
+  return { listPromocional, error, URL_BASE, OnClickHiddenVideo };
 }
