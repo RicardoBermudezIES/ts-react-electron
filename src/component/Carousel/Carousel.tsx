ec5ab11e28c9fe52ad0bd8578f356ab9389239b9
Ricardo Bermudez <ricardo.bermudez@iesonline.com.co>
2021-12-22 11:11:29 -0500

    refactor: se da formato al carousel

--- src/component/Carousel/Carousel.tsx
+++ src/component/Carousel/Carousel.tsx
@@ -1,35 +1,26 @@
-import React from 'react'
+import React from 'react';
+import useCarrousel from '../../Hook/useCarrousel';
 
 type Props = {
-    carouselItems: string[];
-    URL_BASE: string
-}
+  carouselItems: string[];
+  URL_BASE: string;
+};
 
-export default function Carousel( {carouselItems, URL_BASE} : Props) {
-    const [active, setActive] = React.useState(0);
-    let scrollInterval: any = null;
-  
-    React.useEffect(() => {
-      scrollInterval = setTimeout(() => {
-        setActive((active + 1) % carouselItems.length);
-      }, 1000*60*5);
-      return () => clearTimeout(scrollInterval);
-    },[active]);
-  
-    return (
-      <div className="carousel">
-        {carouselItems?.map((item, index) => {
-          const activeClass = active === index ? ' visible' : '';
-            // console.log(`${URL_BASE}/${item}`);
-          return(
-            <img 
-                key={index} 
-                className={`carousel-item${activeClass}`} 
-                src={`${URL_BASE}/${item}`} 
-                alt={item} 
-                /> 
-          )
-             })}
-      </div>
-    );
+export default function Carousel({ carouselItems, URL_BASE }: Props) {
+  const { active } = useCarrousel(carouselItems);
+  return (
+    <div className="carousel">
+      {carouselItems?.map((item, index) => {
+        const activeClass = active === index ? 'visible' : '';
+        return (
+          <img
+            key={item}
+            className={`carousel-item ${activeClass}`}
+            src={`${URL_BASE}/${item}`}
+            alt={item}
+          />
+        );
+      })}
+    </div>
+  );
 }
