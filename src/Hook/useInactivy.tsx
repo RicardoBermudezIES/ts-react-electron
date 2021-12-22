124cee5cce21489af2fa8c7febcc5449d2ffad7a
Ricardo Bermudez <ricardo.bermudez@iesonline.com.co>
2021-12-22 11:10:30 -0500

    refacotr: se da formato al  useInactivy

--- src/Hook/useInactivy.tsx
+++ src/Hook/useInactivy.tsx
@@ -1,38 +1,32 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 export default function useInactivy() {
+  const [inactivy] = useState(60);
+  const [activy, setActivy] = useState(0);
+  const [isVideoPlay, setIsVideoPlay] = useState(false);
 
-    const [inactivy] = useState(180);
-    const [activy, setActivy] = useState(0);
-    const [isVideoPlay, setIsVideoPlay] = useState(false);
-
-
-    useEffect(() => {
-        const timeout = setTimeout(() => {
-            setActivy(activy + 1);
-            checkTime()
-        }, 1000);
-        return () => {
-            clearTimeout(timeout);
-        };
-    }, [inactivy, activy])
-
-
-    useEffect(() => {
-        window.onclick = () => {
-            setActivy(0);
-            setIsVideoPlay(false)
-        };
-    }, [])
-
-
-    function checkTime() {
-        if (activy >= inactivy) {
-            setIsVideoPlay(true)
-            setActivy(0);
-        }
+  function checkTime() {
+    if (activy >= inactivy) {
+      setIsVideoPlay(true);
+      setActivy(0);
     }
+  }
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setActivy(activy + 1);
+      checkTime();
+    }, 1000);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [inactivy, activy]);
 
+  useEffect(() => {
+    window.onclick = () => {
+      setActivy(0);
+      setIsVideoPlay(false);
+    };
+  }, []);
 
-    return {isVideoPlay, setIsVideoPlay}
+  return { isVideoPlay, setIsVideoPlay };
 }
