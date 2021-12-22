fd11c1bc2facbbe8e1a96a4d142b170d426ef7e2
Ricardo Bermudez <ricardo.bermudez@iesonline.com.co>
2021-12-22 11:11:03 -0500

    refactor: se da formato al codigo del Promocional

--- src/component/Promocional/Promocional.tsx
+++ src/component/Promocional/Promocional.tsx
@@ -1,29 +1,30 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 import React from 'react';
 import useInactivy from '../../Hook/useInactivy';
 import useListVideos from '../../Hook/useListVideos';
 import Alert from '../Alert/Alert';
 import Carousel from '../Carousel/Carousel';
 
-
 export default function Promocional() {
   const { isVideoPlay, setIsVideoPlay } = useInactivy();
-  const { listPromocional, error, URL_BASE, OnClickHiddenVideo } = useListVideos(
-    setIsVideoPlay,
-    isVideoPlay,
-  );
+  const {
+    listPromocional,
+    error,
+    URL_BASE,
+    OnClickHiddenVideo,
+  } = useListVideos(setIsVideoPlay, isVideoPlay);
 
   return (
-    <section onClick={OnClickHiddenVideo} className={`${isVideoPlay ? '' : 'hidden'} `}>
-      {
-        listPromocional !== undefined ?
-          <Carousel carouselItems={listPromocional} URL_BASE={URL_BASE} />
-          : <Alert
-          open={error}
-          onClose={() => false }
-          message={error}
-        />
-      }
-
+    // eslint-disable-next-line jsx-a11y/click-events-have-key-events
+    <section
+      onClick={OnClickHiddenVideo}
+      className={`${isVideoPlay ? '' : 'hidden'} `}
+    >
+      {listPromocional !== undefined ? (
+        <Carousel carouselItems={listPromocional} URL_BASE={URL_BASE} />
+      ) : (
+        <Alert open={error} onClose={() => false} message={error} />
+      )}
     </section>
   );
 }
