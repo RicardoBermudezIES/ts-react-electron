97a4e7d659238c490145f8815229ae6272f90a59
Ricardo Bermudez <ricardo.bermudez@iesonline.com.co>
2021-12-28 10:57:12 -0500

    refactor: se amplia los parametros a recibir de las funciones quedan formato

--- src/helpers/format.ts
+++ src/helpers/format.ts
@@ -1,19 +1,19 @@
-export const formatNumber = (number: number) => new Intl.NumberFormat().format(Number(number));
+export const formatNumber = (number: number | string) =>
+  new Intl.NumberFormat().format(Number(number));
 
 export const FormatDay = (date: Date): string => {
   let newDate = new Date(date);
-  return new Intl.DateTimeFormat("en-US").format(newDate);
+  return new Intl.DateTimeFormat('en-US').format(newDate);
 };
 
-export const formatMoney = (number: number): string => {
-  return new Intl.NumberFormat("es-CO", {
-    style: "currency",
-    currency: "COP",
-  }).format(number);
+export const formatMoney = (number: number | string): string => {
+  return new Intl.NumberFormat('es-CO', {
+    style: 'currency',
+    currency: 'COP',
+  }).format(Number(number));
 };
-
 
 export function shortName(name: string): string {
-    const ArrName = name.split(" ");
-    return `${ArrName[0]} ${ArrName[1].slice(0,1)}`;
-};
+  const ArrName = name.split(' ');
+  return `${ArrName[0]} ${ArrName[1].slice(0, 1)}`;
+}
