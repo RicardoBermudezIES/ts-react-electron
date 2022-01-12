03310600b8698aed865ebde0b921588f48b4972d
Ricardo Bermudez <ricardo.bermudez@iesonline.com.co>
2022-01-12 18:02:39 -0500

    feat: Productos.tsx: se cargar los  puntos del bar

--- src/view/Producto.tsx
+++ src/view/Producto.tsx
@@ -95,7 +95,7 @@ export default function Io() {
     )
   );
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  const puntosBar = JSON.parse(localStorage.getItem('puntosDiaxBar')!);
+  const puntosBar = JSON.parse(localStorage.getItem('puntos')!);
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   const user = JSON.parse(localStorage.getItem('user')!);
 
@@ -184,7 +184,7 @@ export default function Io() {
                     component="p"
                     style={{ fontWeight: 'bold' }}
                   >
-                    {formatNumber(puntosBar)}
+                    {formatNumber(puntosBar?.cantidadPuntosDisponibles)}
                   </Typography>
                   <Typography variant="h6" align="right" component="p">
                     Puntos
