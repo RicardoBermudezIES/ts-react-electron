ce716818c8ba4b62804fb226e6180815687f3ed6
Ricardo Bermudez <ricardo.bermudez@iesonline.com.co>
2022-01-13 10:38:22 -0500

    fix: ButtonProductos: se consultan los luntos totales y se condiciona para permitir redimir si no tiene los puntos totales

--- src/component/ButtonProductos.tsx
+++ src/component/ButtonProductos.tsx
@@ -34,7 +34,7 @@ export const ButtonProductos = ({ p }: Props): JSX.Element => {
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   const user = JSON.parse(localStorage.getItem('user')!);
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  const puntos = JSON.parse(localStorage.getItem('puntosDiaxBar')!);
+  const puntos = JSON.parse(localStorage.getItem('puntos')!);
   return (
     <>
       {hasQueque(p?.pk, 'EN_COLA') ? (
@@ -155,7 +155,10 @@ export const ButtonProductos = ({ p }: Props): JSX.Element => {
               </Grid>
               <Grid item lg={12} md={12} sm={12} xs={12} alignItems="flex-end">
                 <Button
-                  disabled={user === null || puntos < p?.puntosParaCanjear}
+                  disabled={
+                    user === null ||
+                    puntos?.cantidadPuntosDisponibles >= p?.puntosParaCanjear
+                  }
                   onClick={() => doRedimir(p?.pk)}
                   variant="contained"
                   color="secondary"
